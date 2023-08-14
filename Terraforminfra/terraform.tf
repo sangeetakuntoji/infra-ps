06c1885a754a2701a1686f3e4dc2648b263f59af
sangeetakuntoji <sangeeta.kuntoji@gmail.com>
2023-08-14 12:10:39 +0200

    updated

--- Terraforminfra/terraform.tf
+++ Terraforminfra/terraform.tf
@@ -1,5 +1,5 @@
 resource "azurerm_static_site" "web" {
   name                = "skywavestaticwebapp01"
   resource_group_name = "skywave-prod"
-  location            = "westus"
+  location            = "eastus2"
 }
